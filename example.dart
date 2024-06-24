d0a71bed37e6df585f6590b223abb4fe07cc08cb
TejasRanjith <tejasranjith035611@gmail.com>
2024-06-24 12:15:49 +0530

    Added New Files

---
+++ example.dart
@@ -0,0 +1,41 @@
+void main() {
+  Map<String, dynamic> studentsList = {
+    // class, list, number of students 
+    "class": "10th",
+    "students": ["Tejas", "Ranjith", "Rahul"],
+    "number_of_students": 20,
+    "isClassAvailable": true
+  };
+
+  print(studentsList['students'][2]);
+}
+
+
+// void main() {
+//   Map<String, dynamic> address = {
+//     'city': 5,
+//     'state': 'Karnataka',
+//     'country': 'India'
+//   };
+
+//   print(address['city']);
+// }
+
+
+
+// void main() {
+//   print('Hello, World!');
+//   int number=10, number2=20;
+  // print("$number $number2");
+  // double number2 = 10.5;
+//   String name = 'Tejas Ranjith';
+//   String letter = 'T';
+//   print("$name , $letter");
+// }
+
+// void main(){
+//     List<int> numberList = [10,20,30,40,50];
+//     print(numberList);
+//     List<String> nameList = ['Tejas','Ranjith'];
+//     print(nameList.length);
+// }
---
+++ hello-world.dart
@@ -0,0 +1,11 @@
+void main() {
+  print('Hello, World!');
+
+  int number = 10;
+  print(number);
+  double number2 = 10.5;
+  print(number2);
+  String name = 'Tejas Ranjith';
+  String letter = 'T';
+  print(name, letter);
+}
