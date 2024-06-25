8b656bcdaf81a33c009de1150bee89412bdc7c99
TejasRanjith <tejasranjith035611@gmail.com>
2024-06-25 15:39:43 +0530

    Finally did it

--- flutter/sample_project/lib/login_page.dart
+++ flutter/sample_project/lib/login_page.dart
@@ -1,10 +1,73 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(0, 131, 108, 1),
+        leading: Icon(Icons.arrow_back, color: Colors.white60),
+        title: Text('WhatsApp',
+            style: TextStyle(
+                fontFamily: "courrier",
+                fontSize: 20.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold)),
+        actions: [
+          // const Card(child:Padding(child:Icons.search, color: Colors.white60,),padding:EdgeInsets.only(right: 20.0)),
+          Icon(Icons.more_vert, color: Colors.white60)
+        ],
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        // children: [
+        //   Text("Hello World ",
+        // style: TextStyle(
+        //   fontSize: 30,
+        // )),
+        //   Text("Hello World ",
+        // style: TextStyle(
+        //   fontSize: 30,
+        // )),
+        //   Text("Hello World ",
+        // style: TextStyle(
+        //   fontSize: 30,
+        // ))
+        // ],
+        children: [
+          ListTile(
+              leading: Icon(
+                Icons.person,
+                size: 40,
+              ),
+              title: Text("TejaBhai999",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              subtitle: Text("Hey there! I am using WhatsApp"),
+              trailing: Column(children: [
+                Text(
+                  "10:00 AM",
+                  style: TextStyle(
+                      color: Colors.green.shade400,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12),
+                ),
+                CircleAvatar(radius: 14,backgroundColor: Colors.green,child:Text(
+                  "1",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ))
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+              ))
+        ],
+      ),
+    );
   }
 }
