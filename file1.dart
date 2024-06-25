4af62bcc57d888e3db083e1ab6c86088c453e2a9
TejasRanjith <tejasranjith035611@gmail.com>
2024-06-25 10:02:40 +0530

    made final changes

---
+++ file1.dart
@@ -0,0 +1,8 @@
+class Sample {
+  int count = 10;
+  int age = 22;
+
+  void printAny(){
+    print("Hello world");
+  }
+}
---
+++ file2.dart
@@ -0,0 +1,9 @@
+
+import 'file1.dart';
+void main(List<String> args){
+
+  Sample obj =Sample();
+  obj.printAny();
+  print(obj.count);
+  print(obj.age);
+}
