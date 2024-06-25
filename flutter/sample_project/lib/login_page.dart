9460f292783ab26eee1d34f494fccce6bb4c7f04
TejasRanjith <tejasranjith035611@gmail.com>
2024-06-25 15:41:41 +0530

    dumb const warning fixed

--- flutter/sample_project/lib/login_page.dart
+++ flutter/sample_project/lib/login_page.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
